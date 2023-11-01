b10865f29771cdbdb5dccb069c6bc088f920787c
Denise <deniselewis1120@gmail.com>
2023-11-01 09:21:25 -0700

    Added scatter plot to compare number of book reviews and ratings

--- main.py
+++ main.py
@@ -7,8 +7,15 @@ with open("books.csv","r") as datafile:
   data = pd.read_csv(datafile,delimiter=",")
 
 # Bubble chart comparing the number of pages in each book to the book’s rating on Goodreads
-fig = sns.scatterplot(x="average_rating", y="# num_pages", size="# num_pages", data=data)
+#fig = sns.scatterplot(x="average_rating", y="# num_pages", size="# num_pages", data=data)
+#plt.xlabel("Average Rating")
+#plt.ylabel("Number of Pages")
+#plt.title("Rating vs Pages")
+#plt.savefig("ratings_vs_pages_bubble_plot.png")
+
+# Scatterplot to compare number of book reviews and ratings
+plt.scatter(data["average_rating"], data["ratings_count"])
 plt.xlabel("Average Rating")
-plt.ylabel("Number of Pages")
-plt.title("Rating vs Pages")
-plt.savefig("ratings_vs_pages_bubble_plot.png")
+plt.ylabel("Number of Reviews")
+plt.title("Ratings vs Reviews")
+plt.savefig( "ratings_vs_reviews_scatter_plot.png")
